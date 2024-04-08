b48ccb6afc3afb3b40184a6a594d839c9235961f
Alex Lewis <ael88@txstate.edu>
2024-04-07 22:01:29 -0500

    Added a few dummy clubs
    purely for demoing purposes

--- flutter_application_1/lib/club_list.dart
+++ flutter_application_1/lib/club_list.dart
@@ -7,6 +7,117 @@ class Clublist extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Clubs'),
       ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            FractionallySizedBox(
+              widthFactor: 1,
+              child: Card(
+                elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Texas State Soccer',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  )
+                ),
+              ),
+            ),
+            SizedBox(height: 15),
+            FractionallySizedBox(
+              widthFactor: 1,
+              child: Card(
+                elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('.EXE: Computer Science Club',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  )
+                ),
+              ),
+            ),
+            SizedBox(height: 15),
+            FractionallySizedBox(
+              widthFactor: 1,
+              child: Card(
+                elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Gamers at Texas State',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  )
+                ),
+              ),
+            ),
+            SizedBox(height: 15),
+            FractionallySizedBox(
+              widthFactor: 1,
+              child: Card(
+                elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('American Marketing Association',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  )
+                ),
+              ),
+            ),
+            SizedBox(height: 15),
+            FractionallySizedBox(
+              widthFactor: 1,
+              child: Card(
+                elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Texas State Esports',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  )
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
