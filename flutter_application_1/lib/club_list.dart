40aee0bcc4f93a4056e31cfa17374dc478ff06d0
alewis3421 <60369233+alewis3421@users.noreply.github.com>
2024-04-13 16:16:07 -0500

    Made all club listing clickable
    they all currently lead to a dummy club profile page

--- flutter_application_1/lib/club_list.dart
+++ flutter_application_1/lib/club_list.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'clubs.dart';
+
 
 class Clublist extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,94 +14,22 @@ class Clublist extends StatelessWidget {
         padding: EdgeInsets.all(20.0),
         child: Column(
           children: <Widget>[
-            FractionallySizedBox(
-              widthFactor: 1,
-              child: Card(
-                elevation: 5,
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Texas State Soccer',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      )
-                    ],
-                  )
-                ),
-              ),
-            ),
-            SizedBox(height: 15),
-            FractionallySizedBox(
-              widthFactor: 1,
-              child: Card(
-                elevation: 5,
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('.EXE: Computer Science Club',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      )
-                    ],
-                  )
-                ),
-              ),
-            ),
-            SizedBox(height: 15),
-            FractionallySizedBox(
-              widthFactor: 1,
-              child: Card(
-                elevation: 5,
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Gamers at Texas State',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      )
-                    ],
-                  )
-                ),
-              ),
-            ),
-            SizedBox(height: 15),
-            FractionallySizedBox(
-              widthFactor: 1,
-              child: Card(
-                elevation: 5,
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('American Marketing Association',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      )
-                    ],
-                  )
-                ),
-              ),
-            ),
-            SizedBox(height: 15),
-            FractionallySizedBox(
-              widthFactor: 1,
-              child: Card(
-                elevation: 5,
+            SizedBox(height: 15,),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Club(
+                    clubID: 1,
+                    clubName: 'Texas State Esports',
+                    clubDescrip: 'we are a silly folk',)
+                  ),
+                );
+              },
+              child: FractionallySizedBox(
+                widthFactor: 1,
+                child: Card(
+                  elevation: 5,
                 child: Padding(
                   padding: const EdgeInsets.all(20.0),
                   child: Column(
@@ -113,11 +44,215 @@ class Clublist extends StatelessWidget {
                     ],
                   )
                 ),
+                ),
               ),
             ),
+            SizedBox(height: 15,),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Club(
+                    clubID: 1,
+                    clubName: 'Texas State Mens Soccer',
+                    clubDescrip: 'we are a silly folk',)
+                  ),
+                );
+              },
+              child: FractionallySizedBox(
+                widthFactor: 1,
+                child: Card(
+                  elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Texas State Mens Soccer',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  )
+                ),
+                ),
+              ),
+            ),
+            SizedBox(height: 15,),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Club(
+                    clubID: 1,
+                    clubName: 'Texas State Womens Soccer',
+                    clubDescrip: 'we are a silly folk',)
+                  ),
+                );
+              },
+              child: FractionallySizedBox(
+                widthFactor: 1,
+                child: Card(
+                  elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Texas State Womens Soccer',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  )
+                ),
+                ),
+              ),
+            ),
+            SizedBox(height: 15,),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Club(
+                    clubID: 1,
+                    clubName: 'Texas State Lacrosse',
+                    clubDescrip: 'we are a silly folk',)
+                  ),
+                );
+              },
+              child: FractionallySizedBox(
+                widthFactor: 1,
+                child: Card(
+                  elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Texas State Lacrosse',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  )
+                ),
+                ),
+              ),
+            ),
+            SizedBox(height: 15,),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Club(
+                    clubID: 1,
+                    clubName: 'Texas State Chess club',
+                    clubDescrip: 'we are a silly folk',)
+                  ),
+                );
+              },
+              child: FractionallySizedBox(
+                widthFactor: 1,
+                child: Card(
+                  elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Texas State Chess club',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  )
+                ),
+                ),
+              ),
+            ),
+            SizedBox(height: 15,),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Club(
+                    clubID: 1,
+                    clubName: 'Texas State Mens Basketball',
+                    clubDescrip: 'we are a silly folk',)
+                  ),
+                );
+              },
+              child: FractionallySizedBox(
+                widthFactor: 1,
+                child: Card(
+                  elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Texas State Mens Basketball',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  )
+                ),
+                ),
+              ),
+            ),
+            SizedBox(height: 15,),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Club(
+                    clubID: 1,
+                    clubName: 'Texas State Womens Basketball',
+                    clubDescrip: 'we are a silly folk',)
+                  ),
+                );
+              },
+              child: FractionallySizedBox(
+                widthFactor: 1,
+                child: Card(
+                  elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Texas State Womens Basketball',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  )
+                ),
+                ),
+              ),
+            )
           ],
         ),
       ),
     );
   }
 }
+
+/* 
+make a club object in this file and then grab from the data base in here 
+hopefully it will iterate through and display what we need it too.
+*/
