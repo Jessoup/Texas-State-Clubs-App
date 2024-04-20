48b03fafd561724c5b684c672bcda8ac41ab7ec4
Laiba Janat <hjw33@txstate.edu>
2024-04-20 18:16:31 -0500

    ClubProfilePage

---
+++ flutter_application_1/lib/clubprofiles.dart
@@ -0,0 +1,40 @@
+//clubprofiles.dart
+import 'package:flutter/material.dart';
+
+class ClubProfilePage extends StatelessWidget {
+  // You can pass club data using the constructor
+  final String clubName;
+  final String clubDescription;
+  final String clubImageUrl; // Optional image URL
+
+  ClubProfilePage({this.clubName, this.clubDescription, this.clubImageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(clubName),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(height: 20),
+          if (clubImageUrl != null)
+            Image.network(clubImageUrl), // Display the image if available
+          SizedBox(height: 20),
+          Text(
+            clubDescription,
+            style: TextStyle(fontSize: 16),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              // Add join functionality later
+            },
+            child: Text('Join Club'),
+          ),
+        ],
+      ),
+    );
+  }
+}
