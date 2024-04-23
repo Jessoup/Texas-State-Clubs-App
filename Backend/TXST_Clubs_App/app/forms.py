2e1c752cf87bc1850de9adaa9a2b28922892d521
Jessoup <brysonmje@gmail.com>
2024-04-23 12:39:27 -0500

    Adding req file and making email display instead of username

--- Backend/TXST_Clubs_App/app/forms.py
+++ Backend/TXST_Clubs_App/app/forms.py
@@ -6,10 +6,10 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = CustomUser
-        fields = ("username", "email")
+        fields = ("username", "email", "first_name", "last_name")
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = CustomUser
-        fields = ("username", "email")
+        fields = ("username", "email", "first_name", "last_name")
