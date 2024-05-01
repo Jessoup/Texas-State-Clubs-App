7895eca424c0930f9176eaca10f0e93454d63250
Laiba Janat <hjw33@txstate.edu>
2024-05-01 13:50:47 -0500

    made changes to make button on bottowm left and fixed
    used stack method

--- flutter_application_1/lib/clubprofiles.dart
+++ flutter_application_1/lib/clubprofiles.dart
@@ -1,13 +1,14 @@
-//clubprofiles.dart
 import 'package:flutter/material.dart';
 
 class ClubProfilePage extends StatelessWidget {
-  // You can pass club data using the constructor
   final String clubName;
   final String clubDescription;
-  final String clubImageUrl; // Optional image URL
+  final String clubImageUrl;
 
-  ClubProfilePage({ required this.clubName, required this.clubDescription, required this.clubImageUrl});
+  ClubProfilePage(
+      {required this.clubName,
+      required this.clubDescription,
+      required this.clubImageUrl});
 
   @override
   Widget build(BuildContext context) {
@@ -15,24 +16,38 @@ class ClubProfilePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(clubName),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+      body: Stack(
         children: <Widget>[
-          SizedBox(height: 20),
-          if (clubImageUrl != "")
-            Image.network(clubImageUrl), // Display the image if available
-          SizedBox(height: 20),
-          Text(
-            clubDescription,
-            style: TextStyle(fontSize: 16),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              SizedBox(height: 20),
+              if (clubImageUrl != "")
+                Image.network(clubImageUrl), // Display the image if available
+              SizedBox(height: 20),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Text(
+                    clubDescription,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ),
+            ],
           ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              // Add join functionality later
-            },
-            child: Text('Join Club'),
-          ),
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: ElevatedButton(
+              onPressed: () {
+                // Add join functionality later
+              },
+              child: Text('Join Club'),
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+              ),
+            ),
+          )
         ],
       ),
     );
